d2ed4daa6f2f9d537e57e9f4ec32a7b20c3885c2
Lunkov_A@utkonos.ru <Aezakmihesoyam321>
2020-09-17 20:43:38 +0300

    delegate getting extensions

--- kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
+++ kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
@@ -1,6 +1,22 @@
 package ru.impression.kotlin_delegate_concatenator
 
 import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty0
+import kotlin.reflect.KProperty1
 
 operator fun <R, T> ReadWriteProperty<R, T>.plus(other: ReadWriteProperty<R, T>) =
     DelegateSum(this, other)
+
+inline fun <reified D> KProperty0<*>.getDelegate(): D? {
+    val delegate = getDelegate()
+    return delegate as? D ?: with(
+        delegate as? DelegateSum<*, *> ?: return null
+    ) { delegate1 as? D ?: delegate2 as? D }
+}
+
+inline fun <R, reified D> KProperty1<R, *>.getDelegate(receiver: R): D? {
+    val delegate = getDelegate(receiver)
+    return delegate as? D ?: with(
+        delegate as? DelegateSum<*, *> ?: return null
+    ) { delegate1 as? D ?: delegate2 as? D }
+}
