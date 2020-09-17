cf5890d227537c1e8c1cb323aef175213e55ddf9
Lunkov_A@utkonos.ru <Aezakmihesoyam321>
2020-09-17 20:53:01 +0300

    made properties accessible

--- kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
+++ kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
@@ -3,11 +3,13 @@ package ru.impression.kotlin_delegate_concatenator
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
+import kotlin.reflect.jvm.isAccessible
 
 operator fun <R, T> ReadWriteProperty<R, T>.plus(other: ReadWriteProperty<R, T>) =
     DelegateSum(this, other)
 
 inline fun <reified D> KProperty0<*>.getDelegateFromSum(): D? {
+    isAccessible = true
     val delegate = getDelegate()
     return delegate as? D ?: with(
         delegate as? DelegateSum<*, *> ?: return null
@@ -15,6 +17,7 @@ inline fun <reified D> KProperty0<*>.getDelegateFromSum(): D? {
 }
 
 inline fun <R, reified D> KProperty1<R, *>.getDelegateFromSum(receiver: R): D? {
+    isAccessible = true
     val delegate = getDelegate(receiver)
     return delegate as? D ?: with(
         delegate as? DelegateSum<*, *> ?: return null
