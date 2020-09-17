2faea3b5bef5a40471713f80f99f6b08524d30b8
Lunkov_A@utkonos.ru <Aezakmihesoyam321>
2020-09-17 20:48:40 +0300

    renamed methods

--- kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
+++ kotlin-delegate-concatenator/src/main/java/ru/impression/kotlin_delegate_concatenator/Ext.kt
@@ -7,14 +7,14 @@ import kotlin.reflect.KProperty1
 operator fun <R, T> ReadWriteProperty<R, T>.plus(other: ReadWriteProperty<R, T>) =
     DelegateSum(this, other)
 
-inline fun <reified D> KProperty0<*>.getDelegate(): D? {
+inline fun <reified D> KProperty0<*>.getDelegateFromSum(): D? {
     val delegate = getDelegate()
     return delegate as? D ?: with(
         delegate as? DelegateSum<*, *> ?: return null
     ) { delegate1 as? D ?: delegate2 as? D }
 }
 
-inline fun <R, reified D> KProperty1<R, *>.getDelegate(receiver: R): D? {
+inline fun <R, reified D> KProperty1<R, *>.getDelegateFromSum(receiver: R): D? {
     val delegate = getDelegate(receiver)
     return delegate as? D ?: with(
         delegate as? DelegateSum<*, *> ?: return null
